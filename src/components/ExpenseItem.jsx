078cf2129d2b06906f3dc2b0c544b2fac0c84d7d
SivaSurender <e.surender14@gmail.com>
2022-11-16 20:30:19 +0530

    Added Expense Item

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,9 @@
 const ExpenseItem = function () {
   return (
     <div>
-      <h1>Hi from expense Item</h1>
+      <h1>Date : 16:Nov:2022</h1>
+      <p>Item Name : Soda</p>
+      <p>Amount : ₹30</p>
     </div>
   );
 };
