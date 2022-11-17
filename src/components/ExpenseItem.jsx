c4190abfc24409526906c2166ca2142d669835db
SivaSurender <e.surender14@gmail.com>
2022-11-17 18:56:33 +0530

    Dynamic Values

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,16 @@
 import "./ExpenseItem.css";
 const ExpenseItem = function () {
+  const expenseDate = new Date().toISOString();
+
+  const expenseDescription = "Soda";
+  const expensePrice = 30;
+
   return (
     <div className="expense-item ">
-      <div>16 Nov 2022</div>
+      <div>{expenseDate}</div>
       <div className="expense-item__description">
-        <h2>Soda</h2>
-        <div className="expense-item__price">₹30</div>
+        <h2>{expenseDescription}</h2>
+        <div className="expense-item__price">₹{expensePrice}</div>
       </div>
     </div>
   );
