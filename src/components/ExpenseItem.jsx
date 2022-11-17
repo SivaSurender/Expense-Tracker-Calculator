dad84fbedb51c946ef5f5454cbdc08e1961874c3
SivaSurender <e.surender14@gmail.com>
2022-11-17 18:43:41 +0530

    Added css for expense item

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,12 @@
+import "./ExpenseItem.css";
 const ExpenseItem = function () {
   return (
-    <div>
-      <h1>Date : 16:Nov:2022</h1>
-      <p>Item Name : Soda</p>
-      <p>Amount : ₹30</p>
+    <div className="expense-item ">
+      <div>16 Nov 2022</div>
+      <div className="expense-item__description">
+        <h2>Soda</h2>
+        <div className="expense-item__price">₹30</div>
+      </div>
     </div>
   );
 };
