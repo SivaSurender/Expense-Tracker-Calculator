f9db25baa2c10e8773f54a668c8b74acf318eb60
SivaSurender <e.surender14@gmail.com>
2022-11-22 19:52:48 +0530

    Mulitple states with useState

--- src/components/NewExpenses/ExpenseForm.jsx
+++ src/components/NewExpenses/ExpenseForm.jsx
@@ -1,19 +1,48 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
 const ExpenseForm = function () {
+  // storing the initial value for both date and amount as string ("")since the values which are read from event.target.value will always be read as string
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setenteredAmount] = useState("");
+  const [enteredDate, setenteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    // storing the entered value in entered Title
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setenteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setenteredDate(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="1" step="1" />
+          <input
+            type="number"
+            min="1"
+            step="1"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-01-01" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-01-01"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
