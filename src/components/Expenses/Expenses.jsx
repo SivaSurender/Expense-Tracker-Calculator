d8f0b62cd76c785afdb903fcd2ba4cc8a2fca2e4
SivaSurender <e.surender14@gmail.com>
2022-11-24 19:21:39 +0530

    Rendering list dynamically

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -13,31 +13,15 @@ const Expenses = function ({ item }) {
     <div>
       <Card className="expenses">
         <ExpensesFilter defaultYear={selectedYear} onFilter={onFilterChange} />
-        <ExpenseItem
-          id={item[0].id}
-          title={item[0].title}
-          amount={item[0].amount}
-          date={item[0].date}
-        />
-        <ExpenseItem
-          id={item[1].id}
-          title={item[1].title}
-          amount={item[1].amount}
-          item
-          date={item[1].date}
-        />
-        <ExpenseItem
-          id={item[2].id}
-          title={item[2].title}
-          amount={item[2].amount}
-          date={item[2].date}
-        />
-        <ExpenseItem
-          id={item[3].id}
-          title={item[3].title}
-          amount={item[3].amount}
-          date={item[3].date}
-        />
+        {/* Rendering arry of objects dynamically with map */}
+        {item.map((eachItem) => (
+          <ExpenseItem
+            key={eachItem.id}
+            title={eachItem.title}
+            amount={eachItem.amount}
+            date={eachItem.date}
+          />
+        ))}
       </Card>
     </div>
   );
