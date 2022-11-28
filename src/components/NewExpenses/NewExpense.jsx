cd9765fbb9b39e12c24b2d3ba10e50e905613ee0
SivaSurender <e.surender14@gmail.com>
2022-11-28 19:28:18 +0530

    Conditional rendering for expenseForm

--- src/components/NewExpenses/NewExpense.jsx
+++ src/components/NewExpenses/NewExpense.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = function (props) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const isEditinghandler = () => {
+    setIsEditing(!isEditing);
+  };
+
   // to get data from parent to child
   const saveExpenseHandler = (prevExpData) => {
     const expenseData = {
@@ -13,10 +20,20 @@ const NewExpense = function (props) {
     // pushing the dat from here to its parent
 
     props.onGetNewExpense(expenseData);
+
+    // close the form once submitted with the help of usestate is editahandler function
+
+    setIsEditing(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpense={saveExpenseHandler} />
+      {!isEditing && <button onClick={isEditinghandler}>Add Expense</button>}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpense={saveExpenseHandler}
+          onCancel={isEditinghandler}
+        />
+      )}
     </div>
   );
 };
